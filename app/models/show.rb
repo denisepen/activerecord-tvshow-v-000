8acb0f70660549132c06c5f5f0860286687209a3
Denise Pendleton <dpendl1@hotmail.com>
2018-04-05 21:19:39 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,13 +1,13 @@
 class Show < ActiveRecord::Base
 
   def self.highest_rating
-    show = Show.maximum("rating")
+    @show = Show.maximum("rating")
 
   end
 
   def self.most_popular_show
 
-    highest_rating.name
+    self.highest_rating.name
   end
 
 end
